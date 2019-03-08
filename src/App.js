1dda7c2f7315ed7c71269e61d4e8cf31c8c26036
Daniel Chernyshev <droque123@gmail.com>
2019-03-08 14:54:30 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MultipleInput from './components/multiple-input'
 
 class App extends Component {
+
+  onChangeMultipleInput(data) {
+    console.log(data)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div style={{width: 400, height: 400, padding: 5, backgroundColor: 'white'}}>
+            <MultipleInput
+                data={["first", "second"]}
+                onChange={this.onChangeMultipleInput}
+                max={5}
+                maxLength={20}
+                placeholder={'Добавить номер'}
+                size='large'
+            />
+          </div>
         </header>
       </div>
     );
---
+++ src/components/multiple-input/index.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './styles.css'
+
+class MultipleInput extends Component {
+
+    lastItem = null;
+
+    state = {
+        newValue: '',
+        data: []
+    };
+
+    componentWillMount() {
+        this.setState({data: this.props.data || []})
+    }
+
+    onChange = async (index, value) => {
+        if (this.props.maxLength !== undefined && value.length > this.props.maxLength) return;
+        if (index === -1)  // add element
+        {
+            this.setState({newValue: value});
+            this.state.data.push(value);
+        }
+        else if (value === "")  // delete element
+        {
+                this.state.data.splice(index, 1)
+        }
+        else  // change element
+        {
+            this.state.data[index] = value;
+        }
+
+        await this.forceUpdate();
+        if (index === -1 && this.lastItem !== null ) {
+            await this.setState({newValue: ''});
+            this.lastItem.focus();
+        }
+
+        this.props.onChange(this.state.data);
+    };
+
+    render() {
+        const {size, data, inputStyles, max, placeholder} = this.props;
+        let inputClassName = 'multiple-input-default';
+        if (size === "large" || size === "default" || size === "small") {
+            inputClassName = 'multiple-input-' + size;
+        }
+
+        return (
+            <div className='multiple-input-container'>
+                {
+                    data.map((text, index) => (
+                        <input className={inputClassName}
+                               style={inputStyles}
+                               key={index}
+                               value={text}
+                               onChange={e => this.onChange(index, e.target.value)}
+                               ref={node => this.lastItem = node}
+                        />
+                    ))
+                }
+                {
+                    (max === undefined || max > this.state.data.length) ?
+                        <input className={inputClassName}
+                               style={this.state.inputStyles}
+                               onChange={e => this.onChange(-1, e.target.value, e.target)}
+                               value={this.state.newValue}
+                               placeholder={placeholder}
+                        />
+                        :
+                        null
+                }
+            </div>
+        );
+    }
+}
+
+export default MultipleInput;
